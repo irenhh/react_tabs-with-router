10d76043db84eba0c6cb0ce61a86d1679d591ab0
irenhh <iriskag@ukr.net>
2020-05-21 11:34:39 +0300

    Updates

---
+++ precache-manifest.79cafb5ca1501938833c8133e1b28bcb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ae3c011e9c025fdf48b103cc0fed35f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "18309f2b02ea3dfe0ab5",
+    "url": "./static/css/main.feb3ef74.chunk.css"
+  },
+  {
+    "revision": "8e67867082d908d1532a",
+    "url": "./static/js/2.73ba29a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.73ba29a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18309f2b02ea3dfe0ab5",
+    "url": "./static/js/main.483ffa15.chunk.js"
+  },
+  {
+    "revision": "8d12f41056caa0d686f6",
+    "url": "./static/js/runtime-main.472ff868.js"
+  }
+]);
